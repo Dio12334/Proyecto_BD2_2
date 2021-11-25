217f3eac3e3606314e4d86038738c9b3cb271d63
Skinde <46155561+Skinde@users.noreply.github.com>
2021-11-25 15:51:51 -0500

    Front end funcional implementado

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask_sqlalchemy import *
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room
-from waitress import serve
-import os
+import json
+from inverted_index import *
+
+
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-socketio = SocketIO(app)
+app.secret_key = ".."
 
+
+file = open("static/data/rpta.json", "r", encoding='UTF-8')
+jsondata = file.read()
 
 @app.route('/')
 def home():
@@ -16,8 +18,15 @@ def home():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
-    print(query)
-    return render_template('results.html')
+    do_query(query, 10)
+    return render_template("results.html")
+
+@app.route('/requestjson', methods=['GET'])
+def jsonreturn():
+    file = open("static/data/rpta.json", "r", encoding='UTF-8')
+    jsondata = file.read()
+    return jsondata
+
 if __name__ == '__main__':
-    app.config['SESSION_TYPE'] = 'filesystem'
-    serve(app,host='192.168.1.18',port=5000)
+    app.secret_key = ".."
+    app.run(host='192.168.1.18',port=5000)
